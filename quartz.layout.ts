acbda65dad6c8e62e4a304e957a334de068efa1c
laura <mail@laura.fm>
2025-09-01 22:32:48 +0200

    Quartz sync: Sep 1, 2025, 10:32 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -41,7 +41,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Explorer(),
   ],
   right: [
-    Component.recent(),
+    Component.RecentNotes()
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
